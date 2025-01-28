33b891478ec9965248be0495acb86f7232f49e7c
Nitin919 <nitinshar999@gmail.com>
2025-01-28 14:13:59 +0000

    Update SocialIcons.constants.jsx

--- src/components/Menu/SocialIcons/SocialIcons.constants.jsx
+++ src/components/Menu/SocialIcons/SocialIcons.constants.jsx
@@ -2,7 +2,7 @@
 
 export const socialLinks = [
 	
-	{name: "Resume", link: 'https://drive.google.com/file/d/15xQmT7kZAKNt42YM1fq4EyWPzrkuA_ir/view?usp=sharing'},
+	{name: "Resume", link: 'https://drive.google.com/file/d/1fUXtj6qmUA-lShI8eeWYr8S2IeVzicwI/view?usp=drive_link'},
 	{ name: 'LinkedIn', link: 'https://www.linkedin.com/in/nitin-sharma-759b202a9' },
 	{ name: 'Github', link: 'https://github.com/Nitin919/' },
 
